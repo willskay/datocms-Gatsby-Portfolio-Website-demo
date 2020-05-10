a27069f24941de95ae481e236a804425af2234bb
willskay <60570333+willskay@users.noreply.github.com>
2020-05-10 10:53:51 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Kayfi Consultancy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
